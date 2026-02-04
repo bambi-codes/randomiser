96bb57b76b1169f6ce8e9c2ed96f8974bf11a3b5
Bambi <sissybambi914@gmail.com>
2026-02-04 16:37:20 +0000

    more setup

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -1,7 +1,12 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const MainPage = () => {
-  return <p>Main Page</p>;
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography>hellooo</Typography>
+    </Box>
+  );
 };
 
 export default MainPage;
